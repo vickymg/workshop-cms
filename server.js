938168cd76a1bab8d14393d9716fd0a076a1da1f
Vicky Gray <vickygray26@hotmail.com>
2016-02-27 12:52:45 +0000

    served index.html

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 var http = require('http');
+var fs = require('fs');
 
-var message = 'I am so happy to be part of the Node Girls workshop!';
+// var message = 'I am so happy to be part of the Node Girls workshop!';
+// var message1 = 'Node js is so much fun!';
+// var message2 = 'Girls rule boys drool.';
 
 function handler (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(message);
-  response.end();
+
+  var endpoint = request.url;
+  console.log(endpoint);
+
+  var method = request.method;
+  console.log(method);
+
+  if (endpoint === '/node') {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(message1);
+    response.end();
+  }
+  else if (endpoint === '/girls') {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(message2);
+    response.end();
+  }
+  else if (endpoint === '/') {
+    response.writeHead(200, {"Content-Type": "text/html"});
+
+    fs.readFile(__dirname + '/public/index.html', function(error, file) {
+     if (error) {
+       console.log(error);
+       return;
+     }
+
+    response.end(file);
+  });
+}
 }
 
 var server = http.createServer(handler);
